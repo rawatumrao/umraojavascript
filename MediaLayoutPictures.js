fc73fbe100dc2c04c3e1bf7610155f8eed292bd7
rawatumrao <rawatumrao@gmail.com>
2024-08-26 14:35:36 +0530

    Create MediaLayoutPictures.js

---
+++ MediaLayoutPictures.js
@@ -0,0 +1,256 @@
+export const mlayout1 =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%2387919a' stroke='%23BBBFC3'></rect>
+  <rect x='4' y='4' width='80' height='55' rx='8' fill='%23BBBEC3'></rect>
+  <svg x='10' y='0' width='300' height='200' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'> 
+  <polygon points='11,6 21,12 11,18' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  <rect x='90' y='5' width='45' height='30' rx='8' fill='%23BBBEC3'></rect>
+  <svg x='102' y='10' width='20' height='20' viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='100' cy='100' r='100' fill='%237b8794' />
+  <path d='M100,100 L100,0 A100,100 0 0,1 180,60 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M100,100 L180,60 A100,100 0 0,1 160,170 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  </svg>";
+export const mlayout1_selected =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%23ffffff' stroke='Aqua'></rect>
+  <rect x='4' y='4' width='80' height='55' rx='8' fill='%23BBBEC3'></rect>
+  <svg x='10' y='0' width='300' height='200' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='11,6 21,12 11,18' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  <rect x='90' y='5' width='45' height='30' rx='8' fill='%23BBBEC3'></rect>
+  <svg x='102' y='10' width='20' height='20' viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='100' cy='100' r='100' fill='%237b8794' />
+  <path d='M100,100 L100,0 A100,100 0 0,1 180,60 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M100,100 L180,60 A100,100 0 0,1 160,170 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  </svg>";
+
+export const mlayout2 =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%2387919a' stroke='%23BBBFC3'></rect>
+  <rect x='4' y='4' width='80' height='55' rx='8' fill='%23BBBFC3'></rect> 
+  <svg x='10' y='0' width='300' height='200' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='11,6 21,12 11,18' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  <rect x='90' y='30' width='45' height='30' rx='8' fill='%23BBBFC3'></rect> 
+  <svg x='102' y='35' width='20' height='20' viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='100' cy='100' r='100' fill='%237b8794' />
+  <path d='M100,100 L100,0 A100,100 0 0,1 180,60 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M100,100 L180,60 A100,100 0 0,1 160,170 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  </svg>";
+
+export const mlayout2_selected =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%23ffffff' stroke='Aqua'> </rect>
+  <rect x='4' y='4' width='80' height='55' rx='8' fill='%23BBBFC3'></rect> 
+  <svg x='10' y='0' width='300' height='200' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='11,6 21,12 11,18' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  <rect x='90' y='30' width='45' height='30' rx='8' fill='%23BBBFC3'></rect> 
+  <svg x='102' y='35' width='20' height='20' viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='100' cy='100' r='100' fill='%237b8794' />
+  <path d='M100,100 L100,0 A100,100 0 0,1 180,60 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M100,100 L180,60 A100,100 0 0,1 160,170 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  </svg>";
+export const mlayout3 =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%2387919a' stroke='%23BBBFC3'></rect>
+  
+  <rect x='3' y='5' width='45' height='30' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='15' y='10' width='20' height='20' viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='100' cy='100' r='100' fill='%237b8794' />
+  <path d='M100,100 L100,0 A100,100 0 0,1 180,60 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M100,100 L180,60 A100,100 0 0,1 160,170 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  
+  <rect x='55' y='5' width='80' height='55' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='65' y='0' width='300' height='200' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='10,6 20,12 10,18' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  </svg>";
+
+export const mlayout3_selected =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%23ffffff' stroke='Aqua'></rect>
+  
+  <rect x='3' y='5' width='45' height='30' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='15' y='10' width='20' height='20' viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='100' cy='100' r='100' fill='%237b8794' />
+  <path d='M100,100 L100,0 A100,100 0 0,1 180,60 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M100,100 L180,60 A100,100 0 0,1 160,170 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  
+  <rect x='55' y='5' width='80' height='55' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='65' y='0' width='300' height='200' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='10,6 20,12 10,18' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  </svg>";
+
+export const mlayout4 =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%2387919a' stroke='%23BBBFC3'></rect>
+  <rect x='3' y='30' width='45' height='30' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='15' y='35' width='20' height='20' viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='100' cy='100' r='100' fill='%237b8794' />
+  <path d='M100,100 L100,0 A100,100 0 0,1 180,60 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M100,100 L180,60 A100,100 0 0,1 160,170 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  <rect x='55' y='5' width='80' height='55' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='65' y='0' width='300' height='200' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='10,6 20,12 10,18' fill='%237b8794' opacity='0.7'/>
+  </svg></svg>";
+
+export const mlayout4_selected =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%23ffffff' stroke='Aqua'></rect>
+  <rect x='3' y='30' width='45' height='30' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='15' y='35' width='20' height='20' viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='100' cy='100' r='100' fill='%237b8794' />
+  <path d='M100,100 L100,0 A100,100 0 0,1 180,60 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M100,100 L180,60 A100,100 0 0,1 160,170 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  <rect x='55' y='5' width='80' height='55' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='65' y='0' width='300' height='200' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='10,6 20,12 10,18' fill='%237b8794' opacity='0.7'/>
+  </svg></svg>";
+
+export const mlayout5 =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%2387919a' stroke='%23BBBFC3'></rect>
+  <rect x='4' y='4' width='80' height='55' rx='8' fill='%23BBBEC3'></rect>
+  <svg x='30' y='18' width='25' height='25' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='50' cy='50' r='50' fill='%237b8794' />
+  <path d='M50,50 L50,0 A50,50 0 0,1 90,30 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M50,50 L90,30 A50,50 0 0,1 80,85 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  <rect x='90' y='5' width='45' height='30' rx='8' fill='%23BBBEC3'></rect>
+  <svg x='95' y='5' width='30' height='30' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='11,6 21,12 11,18' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  </svg>";
+export const mlayout5_selected =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%23ffffff' stroke='Aqua'></rect>
+  <rect x='4' y='4' width='80' height='55' rx='8' fill='%23BBBEC3'></rect>
+  <svg x='30' y='18' width='25' height='25' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='50' cy='50' r='50' fill='%237b8794' />
+  <path d='M50,50 L50,0 A50,50 0 0,1 90,30 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M50,50 L90,30 A50,50 0 0,1 80,85 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  <rect x='90' y='5' width='45' height='30' rx='8' fill='%23BBBEC3'></rect>
+  <svg x='95' y='5' width='30' height='30' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='11,6 21,12 11,18' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  </svg>";
+
+export const mlayout6 =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%2387919a' stroke='%23BBBFC3'></rect>
+  <rect x='4' y='4' width='80' height='55' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='30' y='18' width='25' height='25' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='50' cy='50' r='50' fill='%237b8794' />
+  <path d='M50,50 L50,0 A50,50 0 0,1 90,30 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M50,50 L90,30 A50,50 0 0,1 80,85 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  <rect x='90' y='30' width='45' height='30' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='90' y='30' width='35' height='35' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='11,4 21,10 11,16' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  </svg>";
+
+export const mlayout6_selected =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%23ffffff' stroke='Aqua'></rect>
+  <rect x='4' y='4' width='80' height='55' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='30' y='18' width='25' height='25' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='50' cy='50' r='50' fill='%237b8794' />
+  <path d='M50,50 L50,0 A50,50 0 0,1 90,30 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M50,50 L90,30 A50,50 0 0,1 80,85 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  <rect x='90' y='30' width='45' height='30' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='90' y='30' width='35' height='35' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='11,4 21,10 11,16' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  </svg>";
+export const mlayout7 =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%2387919a' stroke='%23BBBFC3'></rect>
+  <rect x='3' y='5' width='45' height='30' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='5' y='2' width='200' height='135' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='10,3 20,9 10,15' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  
+  <rect x='55' y='5' width='80' height='55' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='82' y='18' width='25' height='25' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='50' cy='50' r='50' fill='%237b8794' />
+  <path d='M50,50 L50,0 A50,50 0 0,1 90,30 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M50,50 L90,30 A50,50 0 0,1 80,85 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  </svg>";
+
+export const mlayout7_selected =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%23ffffff' stroke='Aqua'></rect>
+  <rect x='3' y='5' width='45' height='30' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='5' y='2' width='200' height='135' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='10,3 20,9 10,15' fill='%237b8794' opacity='0.7'/>
+  </svg>
+  <rect x='55' y='5' width='80' height='55' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='82' y='18' width='25' height='25' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='50' cy='50' r='50' fill='%237b8794' />
+  <path d='M50,50 L50,0 A50,50 0 0,1 90,30 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M50,50 L90,30 A50,50 0 0,1 80,85 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  </svg>";
+
+export const mlayout8 =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%2387919a' stroke='%23BBBFC3'></rect>
+  <rect x='3' y='30' width='45' height='30' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='5' y='30' width='200' height='135' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='10,2 20,8 10,14' fill='%237b8794' opacity='0.7'/>
+  </svg> 
+  <rect x='55' y='5' width='80' height='55' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='82' y='18' width='25' height='25' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='50' cy='50' r='50' fill='%237b8794' />
+  <path d='M50,50 L50,0 A50,50 0 0,1 90,30 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M50,50 L90,30 A50,50 0 0,1 80,85 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  </svg>";
+
+
+export const mlayout8_selected =
+  "data:image/svg+xml;utf8,
+  <svg width='100' height='70' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <rect x='0.5' y='0.5' width='139' height='87' rx='3.5' fill='%23ffffff' stroke='Aqua'></rect> 
+  <rect x='3' y='30' width='45' height='30' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='5' y='30' width='200' height='135' viewBox='0 0 140 88' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <polygon points='10,2 20,8 10,14' fill='%237b8794' opacity='0.7'/>
+  </svg> 
+  <rect x='55' y='5' width='80' height='55' rx='8' fill='%23BBBFC3'></rect>
+  <svg x='82' y='18' width='25' height='25' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+  <circle cx='50' cy='50' r='50' fill='%237b8794' />
+  <path d='M50,50 L50,0 A50,50 0 0,1 90,30 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  <path d='M50,50 L90,30 A50,50 0 0,1 80,85 Z' fill='%237b8794' stroke='%23BBBFC3' stroke-width='5'/>
+  </svg>
+  </svg>";
+
