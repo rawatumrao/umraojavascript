b71f71a00b5bf40636bdda6fd10be93ad8a6c861
rawatumrao <rawatumrao@gmail.com>
2024-09-19 02:31:42 +0530

    Update Presenter.jsx

--- Presenter.jsx
+++ Presenter.jsx
@@ -1,43 +1,105 @@
-import React, { useState } from "react";
-import "../mlayout/media.css";
-import { images, headerImage } from "../../../config/imageConstants.js";
-import {
-  MdOutlineKeyboardArrowDown,
-  MdOutlineKeyboardArrowRight,
-} from "react-icons/md";
+import { useRef, useState, useContext, useEffect } from "react";
+import { AppContext } from "../../../contexts/context";
+import "../media/mediaStyle.css";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronLeft,
+  faChevronRight,
+  faAngleDown,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { EVENTS } from "../../../constants/constants.js";
+import { PresenterImages, getParticipantsNumber } from "../../common/presenterImages.jsx";
 
-const Presenter = ({ pLayout }) => {
+const Presenter = ({ pLayout, setSelectedLayout, pexipBroadCastChannel }) => {
+  const {
+    setShowRefresh,
+    showRefresh,
+    updatedShowRefreshVar,
+    presenterLayout,
+    voiceActivated,
+  } = useContext(AppContext);
   const [expanded, setExpanded] = useState(false);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [selectedImage, setSelectedImage] = useState(null);
-
+  const [selectedImage, setSelectedImage] = useState(presenterLayout);
+  const [sortedImages, setSortedImages] = useState([]);
+  const imageContainerRef = useRef(null);
   const navigate = useNavigate();
+  const selectedParticipantsNumber =getParticipantsNumber(presenterLayout);
+  
 
   const handleImageClick = (image) => {
     pLayout(image.layout);
+    setSelectedLayout(image.layout);
+    setSelectedImage((prevImage) => (prevImage === image ? null : image));
+
+    pexipBroadCastChannel.postMessage({
+      event: EVENTS.controlRoomPresenterLayout,
+      info: JSON.parse(JSON.stringify(image.layout)),
+    });
+
+    if (showRefresh === false) {
+      setShowRefresh(true);
+      updatedShowRefreshVar(true);
+    }
+  };
+
+  const handleDoubleClick = (image) => {
     setSelectedImage(image);
+  };
+
+  const sortImages = () => {
+     
+    const greaterOrEqual = PresenterImages.filter(image=> image.participantsNumber >= selectedParticipantsNumber);
+    const lessThan = PresenterImages.filter(image => image.participantsNumber < selectedParticipantsNumber);
+  
+    greaterOrEqual.sort((a,b) => a.participantsNumber - b.participantsNumber);
+    lessThan.sort((a,b)=> a.participantsNumber - b.participantsNumber);
+  
+    setSortedImages([...greaterOrEqual, ...lessThan]);
   };
 
   const toggleExpandCollapse = () => {
     setExpanded(!expanded);
+    if(!expanded){
+      sortImages();
+    }
   };
 
+  useEffect(()=>{
+    if(expanded){
+      sortImages();
+    }
+  }, []);
+
+  //Scrolling fuctions
   const handleNext = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    if (imageContainerRef.current) {
+      imageContainerRef.current.scrollBy({
+        left: 160,
+        behaviot: "smooth",
+      });
+    }
   };
 
   const handlePrev = () => {
-    setCurrentImageIndex(
-      (prevIndex) => (prevIndex - 1 + images.length) % images.length
-    );
+    if (imageContainerRef.current) {
+      imageContainerRef.current.scrollBy({
+        left: -160,
+        behaviot: "smooth",
+      });
+    }
   };
 
   const handleSeeAllClick = () => {
     navigate("/view-all");
   };
+
+
+
+  // const sortedImages = [...PresenterImages].sort((a, b) => {
+  //   return a.participantsNumber - b.participantsNumber;
+  // });
 
   return (
     <div className="expand-collapse-container">
@@ -45,16 +107,29 @@ const Presenter = ({ pLayout }) => {
         {!expanded ? (
           <>
             <span className="expand-button" onClick={toggleExpandCollapse}>
-              <MdOutlineKeyboardArrowRight /> Presenter Layout
+              <FontAwesomeIcon icon={faAngleRight} /> Presenter Layout
             </span>
-            <span className="">
-              <img className="header-image" src={headerImage}></img>
+            <span className="" onClick={toggleExpandCollapse}>
+              <img
+                className={
+                  presenterLayout === "5:7" && voiceActivated === false
+                    ? "header-image disabledImage"
+                    : "header-image"
+                }
+                src={
+                  presenterLayout === "5:7" && voiceActivated
+                    ? PresenterImages[0].selectedImageUrl
+                    : sortedImages.find((item) => {
+                        if (item.layout === presenterLayout) return item;
+                      }).imageUrl
+                }
+              ></img>
             </span>
           </>
         ) : (
           <>
             <span className="collapse-button" onClick={toggleExpandCollapse}>
-              <MdOutlineKeyboardArrowDown /> Presenter Layout
+              <FontAwesomeIcon icon={faAngleDown} /> Presenter Layout
             </span>
             <span className="see-all" onClick={handleSeeAllClick}>
               See All
@@ -69,24 +144,62 @@ const Presenter = ({ pLayout }) => {
             className="nav-arrow left-arrow"
             onClick={handlePrev}
           />
-          <div className="images">
-            {images.slice(currentImageIndex, currentImageIndex + 7).map(
-              (image, index) => (
-                <img
-                  key={index}
-                  src={image.imageUrl}
-                  alt={`Image ${index + 1}`}
-                  onClick={() => handleImageClick(image)}
-                  className="zoom-image"
-                  style={{
-                    border:
-                      selectedImage?.imageUrl === image.imageUrl
-                        ? "2px solid blue"
-                        : "none",
-                  }}
-                />
-              )
-            )}
+          <div className="images" ref={imageContainerRef}>
+            {voiceActivated
+              ? sortedImages.map((image, index) => (
+                  <img
+                    key={index}
+                    src={
+                      selectedImage === image.layout ||
+                      selectedImage?.layout === image.layout ||
+                      image.scope === "Adaptive"
+                        ? image.selectedImageUrl
+                        : image.imageUrl
+                    }
+                    title={image.alt}
+                    alt={image.alt}
+                    onClick={() => handleImageClick(image)}
+                    onDoubleClick={() => handleDoubleClick(image)}
+                    className={`image ${
+                      selectedImage === image.layout ||
+                      selectedImage?.layout === image.layout
+                        ? "zoom-image selectedImage"
+                        : "zoom-image"
+                    }`}
+                  />
+                ))
+              : sortedImages.map((image, index) => (
+                  <img
+                    key={index}
+                    src={
+                      (selectedImage === image.layout ||
+                        selectedImage?.layout === image.layout) &&
+                      image.scope !== "Adaptive"
+                        ? image.selectedImageUrl
+                        : image.imageUrl
+                    }
+                    title={image.alt}
+                    alt={image.alt}
+                    onClick={
+                      image.scope === "Adaptive"
+                        ? () => {}
+                        : () => handleImageClick(image)
+                    }
+                    onDoubleClick={
+                      image.scope === "Adaptive"
+                        ? () => {}
+                        : () => handleDoubleClick(image)
+                    }
+                    className={`image ${
+                      image.scope === "Adaptive"
+                        ? "zoom-image"
+                        : selectedImage === image.layout ||
+                          selectedImage?.layout === image.layout
+                        ? "zoom-image selectedImage"
+                        : "zoom-image"
+                    }`}
+                  />
+                ))}
           </div>
           <FontAwesomeIcon
             icon={faChevronRight}
